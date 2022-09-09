466b1b093a561568ba6279ecffa276774ebe020a
Osinubi Joseph <osinubij6@gmail.com>
2022-09-08 21:10:37 -0700

    sleep

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
-#include <stdio.h>
+{#include <stdio.h>
 
 /**
- *main - prints the phrase "Programming is like building a multilingual puzzle
- *Return: Always 0 (Success)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
+  puts("Programming is like building a multilingual puzzle\n");
   return (0);
 }
-
-
-
+}
