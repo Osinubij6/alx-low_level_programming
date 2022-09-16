268490141d097e273202ab02152e439b25143afa
Osinubi Joseph <osinubij6@gmail.com>
2022-09-15 20:12:31 -0700

    second to the last, or maybe last sef

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,28 +1,36 @@
 #include "main.h"
+void print_integer(int m);
 
 /**
- * print_number - Prints an integer.
- *
- * @n: The integer to be printed.
+ * print_number - a function that prints an integer.
+ * @n: An input integer
+ * Return: Nothing
  */
-
 void print_number(int n)
-
 {
-  unsigned int num = n;
+	if (n == 0)
+		_putchar('0');
+	else if (n < 0)
+	{
+		_putchar('-');
+		print_integer(n * -1);
+	}
+	else
+		print_integer(n);
+}
 
-  /*first check if its negative*/
-  if (n < 0)
+/**
+ * print_integer - A function to priting n
+ * @m: an input unsigned integer
+ * Return: Nothing
+ */
+void print_integer(int m)
+{
+	int i = 1000000000;
 
-    {
-      _putchar('-');
-      num = -num;
-    }
-
-  /*print the first few digits*/
-  if ((num / 10) > 0)
-    print_number(num / 10);
-
-  /*print the last digit*/
-  _putchar((num % 10) + 48);
+	for (; i >= 1; i /= 10)
+		if (m / i != 0)
+		{
+			_putchar((m / i) % 10 + '0');
+		}
 }
