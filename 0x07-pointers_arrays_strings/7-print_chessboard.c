96b8ada3c701e8ebe3da2772a10b1f8edcf460e2
Osinubi Joseph <osinubij6@gmail.com>
2022-09-26 18:25:56 -0700

    I dey go sleep

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
- * print_chessboard - a function that prints a chessboard
- * @a: an array input to print
- * Return: Nothing
+ * print_chessboard - a function that prints a chessboard, 2d array
+ * @a: 2d array of chars
+ * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
-	int i = 0, j;
-
-	for (; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
-		_putchar('\n');
-	}
-}
+  /*variables*/
+	int n, j;
+	n = 0;
+	while (n < 8) /*repeat number*/
+	  {
+	    j = 0;
+	    while (j < 8)
+	      {
+		_putchar ('\n'); /*print mew line*/
+		n++;
+	      }
+	  }
