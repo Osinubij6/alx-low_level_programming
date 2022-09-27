d69d3bca78c7dacfa6c11216303f5a8ad740e44d
OSINUBI Joseph <111043456+Osinubij6@users.noreply.github.com>
2022-09-27 02:11:12 +0100

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,25 +1,18 @@
-#include "main.h"
+#include " main.h"
 
 /**
- * print_chessboard - Print the chessboard
- * @a: array of pieces
- * Return: Nothing.
+ * print_chessboard - a function that prints a chessboard
+ * @a: an array input to print
+ * Return: Nothing
  */
-
 void print_chessboard(char (*a)[8])
-
 {
-int b, c;
+	int i = 0, j;
 
-for (b = 0; b < 8; b++)
-
-{
-for (c = 0; c < 8; c++)
-
-{
-_putchar(a[b][c]);
-}
-
-_putchar('\n');
-}
+	for (; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
 }
