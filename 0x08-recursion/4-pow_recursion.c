ba7c4df95d2d5c6cb5ae15cb530b24135f214061
Osinubi Joseph <osinubij6@gmail.com>
2022-09-28 17:12:00 -0700

    DDOONNEE

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,15 +1,24 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * factorial - a function that returns the factorial of a number.
- * @n: An input integer
- * Return: The factorial of n
+ *_pow_recursion - returns the value of x raised to the power of y
+ *@x: integer
+ *@y: integer
+ *Return: pow recursion
  */
-int factorial(int n)
+
+int _pow_recursion(int x, int y)
+  
 {
-	if (n < 0)
-		return (-1);
-	if (n <= 1)
-		return (1);
-	return (n * factorial(n - 1));
+  if (y < 0) /*Base condition*/
+    
+    {
+      return (-1);    
+    }
+  
+  else if (y != 0)  
+    return (x * _pow_recursion(x, y - 1)); /* Recursive call*/
+  
+  else
+    return (1); 
 }
